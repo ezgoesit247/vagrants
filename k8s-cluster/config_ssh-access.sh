096920cfdcccbf1dbaaca70e6070ccccbb2ae3c5
Brandon R. India <brandon.r.india@gmail.com>
2020-12-05 12:34:08 -0600

    configure eth script to accept one box name parm.

--- k8s-cluster/config_ssh-access.sh
+++ k8s-cluster/config_ssh-access.sh
@@ -1,1 +1,2 @@
-for box in `ls ./.vagrant/machines/`; do ssh root@${box}.k8s-cluster "/etc/init/synchosts.sh"; done
+if [ -z $1 ]; then for box in `ls ./.vagrant/machines/`; do ssh root@${box}.k8s-cluster "/etc/init/synchosts.sh"; done
+else ssh root@${1}.k8s-cluster "/etc/init/synchosts.sh"; fi
