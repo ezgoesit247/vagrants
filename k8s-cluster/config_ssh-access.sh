a54fb2b1ec39007cc878bc26e67ec7f40ab3ffee
Brandon R. India <ezgoesit247@gmail.com>
2020-12-07 15:40:43 -0600

    Changed network config

--- k8s-cluster/config_ssh-access.sh
+++ k8s-cluster/config_ssh-access.sh
@@ -1,2 +1,2 @@
-if [ -z $1 ]; then for box in `ls ./.vagrant/machines/`; do echo -e "\t${box}:" && ssh root@${box}.k8s-cluster "/etc/init/synchosts.sh" && echo; done
-else ssh root@${1}.k8s-cluster "/etc/init/synchosts.sh"; fi
+if [ -z $1 ]; then for box in `ls ./.vagrant/machines/`; do echo -e "\t${box}:" && ssh root@${box}.k8s-cluster "hostname -f && /etc/init/synchosts.sh" && echo; done
+else ssh root@${1}.k8s-cluster "hostname -f && /etc/init/synchosts.sh"; fi
