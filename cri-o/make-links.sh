299042411f5e0aee919487b40ea7e4eaee2abe54
Brandon R. India <ezgoesit247@gmail.com>
2021-01-04 15:17:48 -0600

    crio project

---
+++ cri-o/make-links.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# k8s-cluster
+
+ln -fsn ../empty.sh ./empty.sh
+ln -fsn empty.sh master.sh
+ln -fsn empty.sh node.sh
+ln -fsn empty.sh controller.sh
+ln -fsn ../root_key.sh ./root_key.sh
+ln -fsn ../vagrant_key.sh ./vagrant_key.sh
